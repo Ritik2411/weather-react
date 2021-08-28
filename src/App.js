225d4ee13929e4db89fb684d402fa282e8e994f1
Ritik2411 <73746458+Ritik2411@users.noreply.github.com>
2021-08-29 00:18:41 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Fields from './Components/Fields'
-
+ 
 function App() {
   
   return (
     <div className="App">
-      <div style={{paddingTop:'10px',paddingBottom:'120px',backgroundImage:'url(/Images/blur-background-1187974.jpg)',backgroundSize:'cover',backgroundRepeat:'no-repeat',backgroundPosition:'center',height:'623px'}}>
+      <div style={{paddingTop:'10px',paddingBottom:'120px',backgroundImage:'url(https://images.pexels.com/photos/1187974/pexels-photo-1187974.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260)',backgroundSize:'cover',backgroundRepeat:'no-repeat',backgroundPosition:'center',height:'623px'}}>
         <h1 style={{textAlign:'center',fontWeight:'bold',color:'white',marginTop:'50px',marginBottom:'50px'}}>Live Weather</h1> 
         <Fields/>
       </div>
